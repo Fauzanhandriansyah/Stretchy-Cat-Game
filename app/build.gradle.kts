87109bc0751926688f4dd2305dc0b84a7e78aba9
Zann <141020257+Fauzanhandriansyah@users.noreply.github.com>
2026-08-30 21:00:29 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,6 @@ android {
   testOptions { unitTests { isIncludeAndroidResources = true } }
 }
 
-// Configure the Secrets Gradle Plugin to use .env and .env.example files
-// to match the convention used in Web projects.
 secrets {
   propertiesFileName = ".env"
   defaultPropertiesFileName = ".env.example"
